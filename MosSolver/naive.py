92d4599009409ad6d325b0aa7ae4b2897e21b708
stajama <stajama@yahoo.com>
2017-03-06 22:50:05 -0600

    More PEP8 compliant

--- MosSolver/naive.py
+++ MosSolver/naive.py
@@ -9,7 +9,8 @@ def naiverec(matrix,start=None,end=None,been=None,pathlist=None):
     elif start == end:
         return pathlist
     for coord in [(start[0]-1, start[1]), (start[0]+1,start[1]), (start[0],start[1]+1), (start[0],start[1]-1)]:
-        if not matrix[coord[0]][coord[1]]==0 and not coord[0] <0 and not coord[0] >=len(matrix) and not coord[1]<0 and not coord[1]>=len(matrix[0]) and not coord in been:
+        if not matrix[coord[0]][coord[1]]==0 and not coord[0] <0 and not coord[0] >=len(matrix) and not coord[1]<0 
+        and not coord[1]>=len(matrix[0]) and not coord in been:
             #print(coord)
             newbeen = been + [coord]    
             newpath = pathlist + [coord]
@@ -21,7 +22,7 @@ def naiverec(matrix,start=None,end=None,been=None,pathlist=None):
 
 def naiveimp(matrix):
     def NSEWget(xycoord):
-        return [(xycoord[0]-1, xycoord[1]), (xycoord[0]+1,xycoord[1]), (xycoord[0],xycoord[1]+1), (xycoord[0],xycoord[1]-1)]
+        return [(xycoord[0]-1,xycoord[1]), (xycoord[0]+1,xycoord[1]), (xycoord[0],xycoord[1]+1), (xycoord[0],xycoord[1]-1)]
     start = (0,matrix[0].index(1))
     end = (len(matrix)-1,matrix[-1][matrix[-1].index(1)])
     beenlist=[]
@@ -31,7 +32,8 @@ def naiveimp(matrix):
         print(start)
         checks=NSEWget(start)
         for coord in checks:
-            if not matrix[coord[0]][coord[1]]==1 and not coord[0] <0 and not coord[0] >=len(matrix) and not coord[1]<0 and not coord[1]>=len(matrix[0]) and not coord in beenlist:
+            if not matrix[coord[0]][coord[1]] == 1 and not coord[0] < 0 and not coord[0] >= len(matrix) and not coord[1] < 0 
+            and not coord[1] >= len(matrix[0]) and not coord in beenlist:
                 beenlist.append(coord)
                 outlist.append(coord)
                 start = coord
@@ -41,10 +43,19 @@ def naiveimp(matrix):
                 break
             else:
                 continue
-        if nodelist==[]:
+        if nodelist == []:
             raise ValueError
         start = nodelist.pop()
         outlist = outlist[:outlist.index(start)+1]
     return outlist
 
-#print(naiveimp([[0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1, 1, 1, 1, 0], [0, 1, 0, 0, 0, 0, 1, 0, 1, 0], [0, 1, 1, 1, 0, 1, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1, 0, 0, 1, 0], [0, 1, 1, 1, 1, 1, 0, 1, 1, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 1, 0, 1, 0, 1, 0, 0, 1, 0], [0, 1, 1, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]]))
+print(naiveimp([[0, 0, 0, 1, 0, 0, 0, 0, 0, 0], 
+                [0, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+                [0, 1, 0, 0, 0, 0, 1, 0, 1, 0], 
+                [0, 1, 1, 1, 0, 1, 1, 0, 1, 0], 
+                [0, 1, 0, 1, 0, 1, 0, 0, 1, 0], 
+                [0, 1, 1, 1, 1, 1, 0, 1, 1, 0], 
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], 
+                [0, 1, 0, 1, 0, 1, 0, 0, 1, 0], 
+                [0, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]]))
