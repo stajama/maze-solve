8a41fdc0c47816972a83008a4a8ca84e6d5f896a
stajama <stajama@yahoo.com>
2017-03-07 20:55:20 -0600

    FUCKING WORKS!!!

--- MosSolver/naive.py
+++ MosSolver/naive.py
@@ -9,13 +9,12 @@ def naiverec(matrix,start=None,end=None,been=None,pathlist=None):
     elif start == end:
         return pathlist
     for coord in [(start[0]-1, start[1]), (start[0]+1,start[1]), (start[0],start[1]+1), (start[0],start[1]-1)]:
-        if not matrix[coord[0]][coord[1]]==0 and not coord[0] <0 and not coord[0] >=len(matrix) and not coord[1]<0 
-        and not coord[1]>=len(matrix[0]) and not coord in been:
+        if not matrix[coord[0]][coord[1]] == 0 and not coord[0] < 0 and not coord[0] >= len(matrix) and not coord[1] < 0 and not coord[1] >= len(matrix[0]) and not coord in been:
             #print(coord)
             newbeen = been + [coord]    
             newpath = pathlist + [coord]
             nextstep = naiverec(matrix,coord,end,newbeen,newpath)
-            if not newpath==False:
+            if not newpath == False:
                 return nextstep
     return False
 
@@ -23,39 +22,40 @@ def naiverec(matrix,start=None,end=None,been=None,pathlist=None):
 def naiveimp(matrix):
     def NSEWget(xycoord):
         return [(xycoord[0]-1,xycoord[1]), (xycoord[0]+1,xycoord[1]), (xycoord[0],xycoord[1]+1), (xycoord[0],xycoord[1]-1)]
+
+    def isgoodcoord(coordinate, matr, been):
+        if not coordinate[0] < 0 and not coordinate[0] > len(matr)-1 and not coordinate[1] < 0 and not coordinate[1] >= len(matr[0]) and matr[coordinate[0]][coordinate[1]] == 1 and not coordinate in been:
+            return True
+        return False
+
     start = (0,matrix[0].index(1))
-    end = (len(matrix)-1,matrix[-1][matrix[-1].index(1)])
+    end = (len(matrix)-1,matrix[-1].index(1))
+    #print(end)
     beenlist=[]
     nodelist=[]
     outlist=[start]
     while not start == end:
-        print(start)
+        #print(start)
         checks=NSEWget(start)
         for coord in checks:
-            if not matrix[coord[0]][coord[1]] == 1 and not coord[0] < 0 and not coord[0] >= len(matrix) and not coord[1] < 0 
-            and not coord[1] >= len(matrix[0]) and not coord in beenlist:
+            #print(coord)
+            if isgoodcoord(coord,matrix,beenlist):
+                #print(coord)
+                #print('buh ya!')
                 beenlist.append(coord)
                 outlist.append(coord)
                 start = coord
-                isnode = [matrix[des[0]][des[1]] for des in NSEWget(coord)]
+                #print('bing', [des for des in NSEWget(coord) if isgoodcoord(des,matrix,[])==True])
+                #print(isgoodcoord((10,7),matrix,[]))
+                isnode = [matrix[des[0]][des[1]] for des in NSEWget(coord) if isgoodcoord(des,matrix,[]) == True]
+                #print('isnode', isnode)
                 if isnode.count(1)>=3:
                     nodelist.append(coord)
                 break
-            else:
-                continue
-        if nodelist == []:
-            raise ValueError
-        start = nodelist.pop()
-        outlist = outlist[:outlist.index(start)+1]
+        else:
+            if nodelist == []:
+                raise ValueError
+            start = nodelist.pop()
+            outlist = outlist[:outlist.index(start)+1]
     return outlist
 
-print(naiveimp([[0, 0, 0, 1, 0, 0, 0, 0, 0, 0], 
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
-                [0, 1, 0, 0, 0, 0, 1, 0, 1, 0], 
-                [0, 1, 1, 1, 0, 1, 1, 0, 1, 0], 
-                [0, 1, 0, 1, 0, 1, 0, 0, 1, 0], 
-                [0, 1, 1, 1, 1, 1, 0, 1, 1, 0], 
-                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0], 
-                [0, 1, 0, 1, 0, 1, 0, 0, 1, 0], 
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
-                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]]))
